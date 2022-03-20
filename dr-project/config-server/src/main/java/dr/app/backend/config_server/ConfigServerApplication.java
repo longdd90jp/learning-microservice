98d5d58340c74d0d30b2a6be3e606c0f8cb9d3f2
longdangdjv <long.dang@drjoy.vn>
2022-03-21 00:09:04 +0900

    update

---
+++ dr-project/config-server/src/main/java/dr/app/backend/config_server/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package dr.app.backend.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
